8535394587011f947696781a31eb4ed6ae0f79c7
olivierrousseau1304-dotcom <olivier.rousseau1304@gmail.com>
2025-11-02 15:29:30 +0100

    suppression de commands mode config et mode standard pour meilleur architecture

--- commands.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMANDS_H
-#define COMMANDS_H
-
-#include <Arduino.h>
-
-void processCommand(String input);
-
-#endif
-
--- mode_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef MODE_CONFIG_H
-#define MODE_CONFIG_H
-
-#include <Arduino.h>
-
-void mode_config_start();
-
-#endif
